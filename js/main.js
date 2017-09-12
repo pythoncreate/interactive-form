1d16b2c16f3c53d40ce87f5b8b4a48f112562300
Chris Stuart <crstuart@gmail.com>
2017-09-12 13:03:06 -0400

    new

--- js/main.js
+++ js/main.js
@@ -15,3 +15,27 @@ $("#title").change(function(){
     }
    });
 
+//On selection of the design, sets appropriate color based on the design selected
+$('#design').on('change', function(){
+    if( $(this).val() == 'js puns' ){
+        $('.js').hide();
+        $('.js_puns').show();
+    } else if( $(this).val() == 'heart js' ){
+        $('.js_puns').hide();
+        $('.js').show();
+    } else {
+    	$('.js_puns').show();
+        $('.js').show();
+    }
+});
+
+//Activities
+var $activities = $(".activities >*");
+var $checkedActivities = $activities.checked;
+
+$.each($activities,function(index){
+  //part where the magic happens
+  if $(this).checked{}
+  console.log(index+' checkbox has value' +$(this).val());
+}
+});
